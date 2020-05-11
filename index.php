bb875582c2c7474fd7d19ba6d3414a11db0ace89
mohamedFall <45174412+mohamedFall@users.noreply.github.com>
2020-05-11 20:45:55 +0200

    login form pour la page index
    Le formulaire de connexion

--- index.php
+++ index.php
@@ -4,85 +4,39 @@
 
 
 <!-- PROGRAMMER ICI -->
-<style>
-    body {
-        display: flex;
-        min-height: 100vh;
-        flex-direction: column;
-    }
 
-    main {
-        flex: 1 0 auto;
-    }
-
-    body {
-        background: #fff;
-    }
-
-    .input-field input[type=date]:focus + label,
-    .input-field input[type=text]:focus + label,
-    .input-field input[type=email]:focus + label,
-    .input-field input[type=password]:focus + label {
-        color: #e91e63;
-    }
-
-    .input-field input[type=date]:focus,
-    .input-field input[type=text]:focus,
-    .input-field input[type=email]:focus,
-    .input-field input[type=password]:focus {
-        border-bottom: 2px solid #e91e63;
-        box-shadow: none;
-    }
-</style>
-
-<main>
-    <center>
-        <div class="section"></div>
-
-        <h5 class="indigo-text">Please, login into your account</h5>
-        <div class="section"></div>
-
-        <div class="container">
-            <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
-
-                <form class="col s12" method="post">
-                    <div class='row'>
-                        <div class='col s12'>
-                        </div>
+<div class="section container">
+    <div class="row">
+        <div class="col l3 m2 s12"></div>
+        <div class="col l6 m8 s12">
+            <form action="" method="POST">
+                <div class="card-panel z-depth-5">
+                    <h2 class="center" style="margin-top: 0;">Connexion</h2>
+                    <div class="input-field">
+                        <i class="material-icons prefix">person</i>
+                        <input type="text" placeholder="email ou nom d'utilisateur" name="login_email" focus class="validate">
                     </div>
-
-                    <div class='row'>
-                        <div class='input-field col s12'>
-                            <input class='validate' type='email' name='email' id='email' />
-                            <label for='email'>Enter your email</label>
-                        </div>
+                    <div class="input-field">
+                        <i class="material-icons prefix">lock</i>
+                        <input type="password" placeholder="mot de passe" name="login_password" class="validate">
                     </div>
-
-                    <div class='row'>
-                        <div class='input-field col s12'>
-                            <input class='validate' type='password' name='password' id='password' />
-                            <label for='password'>Enter your password</label>
-                        </div>
-                        <label style='float: right;'>
-                            <a class='pink-text' href='#!'><b>Forgot Password?</b></a>
+                    <p>
+                        <label>
+                            <input type="checkbox" name="login_checkbox">
+                            <span>Se souvenir de moi</span>
                         </label>
-                    </div>
-
-                    <br />
-                    <center>
-                        <div class='row'>
-                            <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect indigo'>Login</button>
-                        </div>
-                    </center>
-                </form>
-            </div>
+                    </p>
+                    <input type="submit" name="formlogin" value="Se connecter" class="btn blue left col s12">
+                    <p class="left">Ëtes-vous nouveau? <a href="#" class="blue-text">Inscripton</a></p>
+                    <p class="right"><a class="blue-text" href="#">Mot de passe oublié?</a></p>
+                    <div class="clearfix"></div>
+                </div>
+            </form>
         </div>
-        <a href="#!">Create account</a>
-    </center>
+        <div class="col l3 m2 s12"></div>
+    </div>
+</div>
 
-    <div class="section"></div>
-    <div class="section"></div>
-</main>
 
 
 
